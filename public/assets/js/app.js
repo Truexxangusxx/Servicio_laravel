dca3b7089e1ca8dde52828f44e8f02653b06aa7c
Carlos Medina <cmedina@electrodata.com.pe>
2015-11-19 16:28:53 -0500

    ajustando asignacion

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -42,3 +42,85 @@ app.controller("first_controller", function($scope, $http,$window){
 		
 		
 });
+
+
+
+app.controller("asignar_colaborador", function($scope, $http,$window){
+		$scope.asignacion = {};
+		$scope.empresas = {};
+		$scope.listas = {};
+		$scope.usuarios = {};
+		
+		$scope.sesion={};
+		$http({
+		        url: "/usuario_logeado",
+		        method: "GET"
+		    })
+        		.success(function(data){
+                    $scope.sesion=data;
+        		})
+        		.error(function(err){
+        			console.log(err);
+        	});
+		
+		
+		$http({
+		        url: "/empresas",
+		        method: "GET"
+		    })
+        		.success(function(data){
+                    $scope.empresas=data;
+        		})
+        		.error(function(err){
+        			console.log(err);
+        		});
+        		
+        		
+        $http({
+		        url: "/users",
+		        method: "GET",
+		        params: {"colaborador":1}
+		    })
+        		.success(function(data){
+                    $scope.usuarios=data;
+        		})
+        		.error(function(err){
+        			console.log(err);
+        		});
+		
+		
+		
+		
+		$scope.registrar_asignacion=function(){
+		    
+		    $http({
+		        url: "/asignacions/create",
+		        method: "GET",
+		        params: $scope.nueva_asignacion
+		    })
+        		.success(function(data){
+                    $window.location.href ="/views/listar_asignaciones.html";
+        		})
+        		.error(function(err){
+        			console.log(err);
+        			error(err.Message);
+        		});
+		}
+		
+		$scope.listar_listas=function(){
+		    
+		    $http({
+		        url: "/listas",
+		        method: "GET",
+		        params: {"empresa_id":$scope.empresa}
+		    })
+        		.success(function(data){
+                    $scope.listas=data;
+        		})
+        		.error(function(err){
+        			alert(err);
+        		});
+		}
+		
+		
+});
--- resources/views/asignar_colaborador.blade.php
+++ resources/views/asignar_colaborador.blade.php
@@ -1,120 +1,11 @@
-<!DOCTYPE html>
-<html ng-app="colas">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de colas</title>
 
-    <link href="{{ URL::asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ URL::asset('assets/css/personalizado.css') }}" rel="stylesheet" type="text/css">
- 	<script src="{{ URL::asset('assets/js/jquery-1.11.3.min.js') }}"></script>
-    <script src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ URL::asset('assets/js/angular.min.js') }}"></script>
-    
-	
-<script>
+@extends('layouts.master')
 
-angular.module("colas",[])
-	.controller("asignacion_controller", function($scope, $http,$window){
-		$scope.asignacion = {};
-		$scope.empresas = {};
-		$scope.listas = {};
-		$scope.usuarios = {};
-		
-		$scope.sesion={};
-		$http({
-		        url: "/usuario_logeado",
-		        method: "GET"
-		    })
-        		.success(function(data){
-                    $scope.sesion=data;
-        		})
-        		.error(function(err){
-        			console.log(err);
-        	});
-		
-		
-		$http({
-		        url: "/empresas",
-		        method: "GET"
-		    })
-        		.success(function(data){
-                    $scope.empresas=data;
-        		})
-        		.error(function(err){
-        			console.log(err);
-        		});
-        		
-        		
-        $http({
-		        url: "/users",
-		        method: "GET",
-		        params: {"colaborador":1}
-		    })
-        		.success(function(data){
-                    $scope.usuarios=data;
-        		})
-        		.error(function(err){
-        			console.log(err);
-        		});
-		
-		
-		
-		
-		$scope.registrar_asignacion=function(){
-		    
-		    $http({
-		        url: "/asignacions/create",
-		        method: "GET",
-		        params: $scope.nueva_asignacion
-		    })
-        		.success(function(data){
-                    $window.location.href ="/views/listar_asignaciones.html";
-        		})
-        		.error(function(err){
-        			console.log(err);
-        			error(err.Message);
-        		});
-		}
-		
-		$scope.listar_listas=function(){
-		    
-		    $http({
-		        url: "/listas",
-		        method: "GET",
-		        params: {"empresa_id":$scope.empresa}
-		    })
-        		.success(function(data){
-                    $scope.listas=data;
-        		})
-        		.error(function(err){
-        			alert(err);
-        		});
-		}
-		
-		
-	});
+@section('title', 'Registro de nueva asignacion')
 
-</script>
+@section('content')
 
-</head>
-
-<body ng-controller="asignacion_controller">
-<div ng-include="'nav'"></div>
-<div class = "container">
-
-<div class = "panel panel-primary">
-   <div class = "panel-heading">
-      <h3 class = "panel-title">Registro de nueva asignacion</h3>
-   </div>
-   <div class = "panel-body">
-
-
-	
-	
-<div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">	
-    <form role="form">
+  <form role="form">
     
         <select class = "form-control" ng-model="empresa" ng-options="item.id as item.nombre for item in empresas track by item.id" ng-change="listar_listas()">
             <option value="">Seleccione una empresa</option>
@@ -122,20 +13,11 @@ angular.module("colas",[])
         <select class = "form-control" ng-model="nueva_asignacion.lista_id" ng-options="item.id as item.nombre for item in listas track by item.id">
             <option value="">Seleccione una empresa</option>
         </select><br/>
-    	<select class = "form-control" ng-model="nueva_asignacion.user_id" ng-options="item.id as item.name for item in usuarios track by item.id">
+        <select class = "form-control" ng-model="nueva_asignacion.user_id" ng-options="item.id as item.name for item in usuarios track by item.id">
             <option value="">Seleccione una empresa</option>
         </select><br/>
-    	<a href = "#" class = "btn btn-default" role = "button" ng-click="registrar_asignacion()">Registrar</a>
-    	<a href = "app_listar_asignaciones" class = "btn btn-default" role = "button" >Regresar</a>
+        <a href = "#" class = "btn btn-default" role = "button" ng-click="registrar_asignacion()">Registrar</a>
+        <a href = "app_listar_asignaciones" class = "btn btn-default" role = "button" >Regresar</a>
     </form>
-</div>
 
-
-
-</div>
-</div>
-
-</div>	
-<div id="footer" ng-include="'footer'"></div>
-</body>
-</html>
+@stop
