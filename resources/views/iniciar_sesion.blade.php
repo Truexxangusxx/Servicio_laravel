bacc2c774a2eebe1bc14313467a567f119253941
Carlos Medina <cmedina@electrodata.com.pe>
2015-11-19 16:22:03 -0500

    probando master page laravel

--- resources/views/iniciar_sesion.blade.php
+++ resources/views/iniciar_sesion.blade.php
@@ -1,20 +1,19 @@
 
 @extends('layouts.master')
 
-@section('title', 'Page Title')
-
-
+@section('title', 'Iniciar sesion')
 
 @section('content')
-  <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3"> 
-    <form role="form">
-    
-      <input type="text" ng-model="user.email" class = "form-control" placeholder = "Correo"/><br/>
-      <input type="password"  ng-model="user.password" class = "form-control" placeholder = "Password"/><br/>
-      
-      <a href = "#" class = "btn btn-default" role = "button" ng-click="iniciar_sesion()">Ingresar</a>
-      <a href = "#" class = "btn btn-default" role = "button" >Cancelar</a>
-    </form>
-  </div>
-@stop
 
+  
+  <form role="form">
+    
+    <input type="text" ng-model="user.email" class = "form-control" placeholder = "Correo"/><br/>
+    <input type="password"  ng-model="user.password" class = "form-control" placeholder = "Password"/><br/>
+      
+    <a href = "#" class = "btn btn-default" role = "button" ng-click="iniciar_sesion()">Ingresar</a>
+    <a href = "#" class = "btn btn-default" role = "button" >Cancelar</a>
+  </form>
+  
+
+@stop
